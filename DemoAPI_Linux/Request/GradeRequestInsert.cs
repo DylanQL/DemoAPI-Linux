5d81e1b8337d0f7096dcb1df9795e12ebee0cef7
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-11-20 20:00:25 -0500

    Feat: Creando controller de grade con su request y response

---
+++ DemoAPI_Linux/Request/GradeRequestInsert.cs
@@ -0,0 +1,7 @@
+namespace DemoAPI_Linux.Request;
+
+public class GradeRequestInsert
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ DemoAPI_Linux/Response/GradeResponseGet.cs
@@ -0,0 +1,8 @@
+namespace DemoAPI_Linux.Response;
+
+public class GradeResponseGet
+{
+    public int GradeId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
